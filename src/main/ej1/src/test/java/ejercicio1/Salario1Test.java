0723dbe7b35062579996be72d4058b2a4486c5ec
Alexia Marin <alexiad.marinm@gmail.com>
2023-10-04 19:02:53 -0700

    Parcial1-Commit2

--- src/main/ej1/src/test/java/ejercicio1/Salario1Test.java
+++ src/main/ej1/src/test/java/ejercicio1/Salario1Test.java
@@ -1,5 +1,32 @@
 package ejercicio1;
+import org.example.Salarios1;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Salario1Test {
+    @Test
+    public void testCalcularDescuentoSinDescuento() {
+
+        Salarios1 salarioManager = spy(new Salarios1());
+        double salario = 1500.0;
+        doReturn(0.0).when(salarioManager).calcularDescuento(salario);
+
+        double descuento = salarioManager.calcularDescuento(salario);
+        verify(salarioManager, times(1)).calcularDescuento(salario);
+        assertEquals(0.0, descuento);
+    }
+    @Test
+    public void testCalcularDescuentoDescuento5PorCiento() {
+        Salarios1 salarioManager = spy(new Salarios1());
+        double salario = 2100.0;
+
+        doReturn(0.05 * salario).when(salarioManager).calcularDescuento(salario);
+
+        double descuento = salarioManager.calcularDescuento(salario);
+        verify(salarioManager, times(1)).calcularDescuento(salario);
+
+        assertEquals(0.05 * salario, descuento);
+    }
 
 }
